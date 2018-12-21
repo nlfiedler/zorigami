0d377fd791d84193b6fbd84669d5774c58c8c241
Nathan Fiedler <nathanfiedler@fastmail.fm>
2018-12-20 20:51:11 -0800

    Add some basic code and tests
    npm test passes

---
+++ test/core.js
@@ -0,0 +1,23 @@
+//
+// Copyright (c) 2018 Nathan Fiedler
+//
+const { assert } = require('chai')
+const { describe, it } = require('mocha')
+const core = require('../lib/core')
+
+describe('Core Functionality', function () {
+  describe('unique identifier', function () {
+    it('should return a UUIDv5 for user and host', function () {
+      const uniqId = core.generateUniqueId('charlie', 'localhost')
+      assert.equal(uniqId, '747267d5-6e70-5711-8a9a-a40c24c1730f')
+    })
+  })
+
+  describe('bucket name', function () {
+    it('should generate a 58 character string', function () {
+      const uniqId = core.generateUniqueId('charlie', 'localhost')
+      const bucket = core.generateBucketName(uniqId)
+      assert.equal(bucket.length, 58)
+    })
+  })
+})
