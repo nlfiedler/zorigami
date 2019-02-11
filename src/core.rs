ad6e1b8a8344c8cc4af1d86387c4745a01f15afb
Nathan Fiedler <nathanfiedler@fastmail.fm>
2019-02-10 20:54:11 -0800

    Fix the chunking to include algo prefix
    cargo test passes

--- src/core.rs
+++ src/core.rs
@@ -128,7 +128,8 @@ pub fn find_file_chunks(infile: &Path, size: u32) -> io::Result<Vec<Chunk>> {
     let mut results = Vec::new();
     for entry in chunker {
         let end = entry.offset + entry.length;
-        let digest = hex_digest(Algorithm::SHA256, &mmap[entry.offset..end]);
+        let mut digest = String::from("sha256-");
+        digest.push_str(&hex_digest(Algorithm::SHA256, &mmap[entry.offset..end]));
         results.push(Chunk { digest, offset: entry.offset, length: entry.length })
     }
     Ok(results)
@@ -223,16 +224,22 @@ mod tests {
         assert_eq!(results.len(), 6);
         assert_eq!(results[0].offset, 0);
         assert_eq!(results[0].length, 22366);
+        assert_eq!(results[0].digest, "sha256-103159aa68bb1ea98f64248c647b8fe9a303365d80cb63974a73bba8bc3167d7");
         assert_eq!(results[1].offset, 22366);
         assert_eq!(results[1].length, 8282);
+        assert_eq!(results[1].digest, "sha256-c95e0d6a53f61dc7b6039cfb8618f6e587fc6395780cf28169f4013463c89db3");
         assert_eq!(results[2].offset, 30648);
         assert_eq!(results[2].length, 16303);
+        assert_eq!(results[2].digest, "sha256-e03c4de56410b680ef69d8f8cfe140c54bb33f295015b40462d260deb9a60b82");
         assert_eq!(results[3].offset, 46951);
         assert_eq!(results[3].length, 18696);
+        assert_eq!(results[3].digest, "sha256-bd1198535cdb87c5571378db08b6e886daf810873f5d77000a54795409464138");
         assert_eq!(results[4].offset, 65647);
         assert_eq!(results[4].length, 32768);
+        assert_eq!(results[4].digest, "sha256-5c8251cce144b5291be3d4b161461f3e5ed441a7a24a1a65fdcc3d7b21bfc29d");
         assert_eq!(results[5].offset, 98415);
         assert_eq!(results[5].length, 11051);
+        assert_eq!(results[5].digest, "sha256-a566243537738371133ecff524501290f0621f786f010b45d20a9d5cf82365f8");
         Ok(())
     }
 
@@ -243,10 +250,13 @@ mod tests {
         assert_eq!(results.len(), 3);
         assert_eq!(results[0].offset, 0);
         assert_eq!(results[0].length, 32857);
+        assert_eq!(results[0].digest, "sha256-5a80871bad4588c7278d39707fe68b8b174b1aa54c59169d3c2c72f1e16ef46d");
         assert_eq!(results[1].offset, 32857);
         assert_eq!(results[1].length, 16408);
+        assert_eq!(results[1].digest, "sha256-13f6a4c6d42df2b76c138c13e86e1379c203445055c2b5f043a5f6c291fa520d");
         assert_eq!(results[2].offset, 49265);
         assert_eq!(results[2].length, 60201);
+        assert_eq!(results[2].digest, "sha256-0fe7305ba21a5a5ca9f89962c5a6f3e29cd3e2b36f00e565858e0012e5f8df36");
         Ok(())
     }
 
@@ -257,8 +267,10 @@ mod tests {
         assert_eq!(results.len(), 2);
         assert_eq!(results[0].offset, 0);
         assert_eq!(results[0].length, 32857);
+        assert_eq!(results[0].digest, "sha256-5a80871bad4588c7278d39707fe68b8b174b1aa54c59169d3c2c72f1e16ef46d");
         assert_eq!(results[1].offset, 32857);
         assert_eq!(results[1].length, 76609);
+        assert_eq!(results[1].digest, "sha256-5420a3bcc7d57eaf5ca9bb0ab08a1bd3e4d89ae019b1ffcec39b1a5905641115");
         Ok(())
     }
 }
