c826195dbcc6ec2c6631b9a896e53d4c624c5941
Nathan Fiedler <nathanfiedler@fastmail.fm>
2019-04-16 09:36:39 -0700

    Finish testing snapshots and finding changes
    cargo test passes

--- tests/engine_test.rs
+++ tests/engine_test.rs
@@ -9,132 +9,6 @@ use zorigami::core::*;
 use zorigami::database::*;
 use zorigami::engine::*;
 
-fn make_entry(pathname: &str, digest: &str) -> TreeEntry {
-    let path = Path::new(pathname);
-    let sha1 = Checksum::SHA1(digest.to_owned());
-    let tref = TreeReference::FILE(sha1);
-    TreeEntry::new(&path, tref).unwrap()
-}
-
-#[test]
-fn test_tree_walker() {
-    // create a clean database for each test
-    let db_path = "tmp/test/engine/walker";
-    let _ = fs::remove_dir_all(db_path);
-    let dbase = Database::new(Path::new(db_path)).unwrap();
-    // create the a/b/c tree object
-    let entry1 = make_entry(
-        "./test/fixtures/lorem-ipsum.txt",
-        "b14c4909c3fce2483cd54b328ada88f5ef5e8f96",
-    );
-    let entry2 = make_entry(
-        "./test/fixtures/SekienAkashita.jpg",
-        "4c009e44fe5794df0b1f828f2a8c868e66644964",
-    );
-    let tree = Tree::new(vec![entry1, entry2], 2);
-    let tree_c = tree.checksum();
-    let result = dbase.insert_tree(&tree_c, &tree);
-    assert!(result.is_ok());
-
-    // create the a/b tree object
-    let tree_path = Path::new("./test/fixtures");
-    let tref_c = TreeReference::TREE(tree_c.clone());
-    let result = TreeEntry::new(&tree_path, tref_c);
-    let entry_c = result.unwrap();
-    let entry1 = make_entry(
-        "./test/fixtures/lorem-ipsum.txt",
-        "b14c4909c3fce2483cd54b328ada88f5ef5e8f96",
-    );
-    let entry2 = make_entry(
-        "./test/fixtures/SekienAkashita.jpg",
-        "4c009e44fe5794df0b1f828f2a8c868e66644964",
-    );
-    let tree = Tree::new(vec![entry_c, entry1, entry2], 4);
-    let tree_b = tree.checksum();
-    let result = dbase.insert_tree(&tree_b, &tree);
-    assert!(result.is_ok());
-
-    // create the a tree object
-    let tref_b = TreeReference::TREE(tree_b.clone());
-    let result = TreeEntry::new(&tree_path, tref_b);
-    let entry_b = result.unwrap();
-    let entry1 = make_entry(
-        "./test/fixtures/lorem-ipsum.txt",
-        "b14c4909c3fce2483cd54b328ada88f5ef5e8f96",
-    );
-    let entry2 = make_entry(
-        "./test/fixtures/SekienAkashita.jpg",
-        "4c009e44fe5794df0b1f828f2a8c868e66644964",
-    );
-    let tree = Tree::new(vec![entry_b, entry1, entry2], 6);
-    let tree_a = tree.checksum();
-    let result = dbase.insert_tree(&tree_a, &tree);
-    assert!(result.is_ok());
-
-    // walk the tree starting at a
-    let walker = TreeWalker::new(&dbase, Path::new("a"), tree_a);
-    let collected: Vec<Result<ChangedFile, Error>> = walker.collect();
-    assert_eq!(collected.len(), 6);
-    assert_eq!(
-        collected[0].as_ref().unwrap().path,
-        PathBuf::from("a/SekienAkashita.jpg")
-    );
-    assert_eq!(
-        collected[1].as_ref().unwrap().path,
-        PathBuf::from("a/lorem-ipsum.txt")
-    );
-    assert_eq!(
-        collected[2].as_ref().unwrap().path,
-        PathBuf::from("a/fixtures/SekienAkashita.jpg")
-    );
-    assert_eq!(
-        collected[3].as_ref().unwrap().path,
-        PathBuf::from("a/fixtures/lorem-ipsum.txt")
-    );
-    assert_eq!(
-        collected[4].as_ref().unwrap().path,
-        PathBuf::from("a/fixtures/fixtures/SekienAkashita.jpg")
-    );
-    assert_eq!(
-        collected[5].as_ref().unwrap().path,
-        PathBuf::from("a/fixtures/fixtures/lorem-ipsum.txt")
-    );
-
-    // walk the tree starting at a/b
-    let walker = TreeWalker::new(&dbase, Path::new("b"), tree_b);
-    let collected: Vec<Result<ChangedFile, Error>> = walker.collect();
-    assert_eq!(collected.len(), 4);
-    assert_eq!(
-        collected[0].as_ref().unwrap().path,
-        PathBuf::from("b/SekienAkashita.jpg")
-    );
-    assert_eq!(
-        collected[1].as_ref().unwrap().path,
-        PathBuf::from("b/lorem-ipsum.txt")
-    );
-    assert_eq!(
-        collected[2].as_ref().unwrap().path,
-        PathBuf::from("b/fixtures/SekienAkashita.jpg")
-    );
-    assert_eq!(
-        collected[3].as_ref().unwrap().path,
-        PathBuf::from("b/fixtures/lorem-ipsum.txt")
-    );
-
-    // walk the tree starting at a/b/c
-    let walker = TreeWalker::new(&dbase, Path::new("c"), tree_c);
-    let collected: Vec<Result<ChangedFile, Error>> = walker.collect();
-    assert_eq!(collected.len(), 2);
-    assert_eq!(
-        collected[0].as_ref().unwrap().path,
-        PathBuf::from("c/SekienAkashita.jpg")
-    );
-    assert_eq!(
-        collected[1].as_ref().unwrap().path,
-        PathBuf::from("c/lorem-ipsum.txt")
-    );
-}
-
 #[test]
 fn test_basic_snapshots() -> Result<(), Error> {
     // create a clean database for each test
@@ -331,4 +205,101 @@ fn test_snapshot_types() -> Result<(), Error> {
     Ok(())
 }
 
-// TODO: copy each of the tests from test/engine.ts that test findChangedFiles()
+#[test]
+fn test_snapshot_ignore_links() -> Result<(), Error> {
+    // create a clean database for each test
+    let db_path = "tmp/test/engine/ignore_links/rocksdb";
+    let _ = fs::remove_dir_all(db_path);
+    let dbase = Database::new(Path::new(db_path)).unwrap();
+    let basepath = "tmp/test/engine/ignore_links/fixtures";
+    let _ = fs::remove_dir_all(basepath);
+    fs::create_dir_all(basepath)?;
+    let bbb: PathBuf = [basepath, "bbb"].iter().collect();
+    let ccc: PathBuf = [basepath, "ccc", "ccc.txt"].iter().collect();
+    fs::create_dir(ccc.parent().unwrap())?;
+    fs::write(&bbb, b"bored baby baboons bathing")?;
+    fs::write(&ccc, b"crazy cat clawing chairs")?;
+    // take a snapshot of the test data
+    let snap1_sha = take_snapshot(Path::new(basepath), None, &dbase)?;
+    let snapshot1 = dbase.get_snapshot(&snap1_sha)?.unwrap();
+    assert_eq!(snapshot1.file_count, 2);
+    // replace the files and directories with links
+    let mmm: PathBuf = [basepath, "mmm.txt"].iter().collect();
+    fs::write(&mmm, b"morose monkey munching muffins")?;
+    fs::remove_file(&bbb)?;
+    fs::remove_dir_all(ccc.parent().unwrap())?;
+    let ccc: PathBuf = [basepath, "ccc"].iter().collect();
+    // cfg! macro doesn't work for this case it seems so we have this
+    // redundant use of the cfg directive instead
+    {
+        #[cfg(target_family = "unix")]
+        use std::os::unix::fs;
+        #[cfg(target_family = "windows")]
+        use std::os::windows::fs;
+        #[cfg(target_family = "unix")]
+        fs::symlink("mmm.txt", &bbb)?;
+        #[cfg(target_family = "unix")]
+        fs::symlink("mmm.txt", &ccc)?;
+        #[cfg(target_family = "windows")]
+        fs::symlink_file("mmm.txt", &bbb)?;
+        #[cfg(target_family = "windows")]
+        fs::symlink_file("mmm.txt", &ccc)?;
+    }
+    let snap2_sha = take_snapshot(Path::new(basepath), Some(snap1_sha.clone()), &dbase)?;
+    // compute the differences
+    let iter = find_changed_files(&dbase, snap1_sha.clone(), snap2_sha.clone())?;
+    let changed: Vec<Result<ChangedFile, Error>> = iter.collect();
+    assert_eq!(changed.len(), 1);
+    assert_eq!(changed[0].as_ref().unwrap().path, Path::new("./mmm.txt"));
+    Ok(())
+}
+
+#[test]
+fn test_snapshot_was_links() -> Result<(), Error> {
+    // create a clean database for each test
+    let db_path = "tmp/test/engine/was_links/rocksdb";
+    let _ = fs::remove_dir_all(db_path);
+    let dbase = Database::new(Path::new(db_path)).unwrap();
+    let basepath = "tmp/test/engine/was_links/fixtures";
+    let _ = fs::remove_dir_all(basepath);
+    fs::create_dir_all(basepath)?;
+    let mmm: PathBuf = [basepath, "mmm.txt"].iter().collect();
+    fs::write(&mmm, b"morose monkey munching muffins")?;
+    let bbb: PathBuf = [basepath, "bbb"].iter().collect();
+    let ccc: PathBuf = [basepath, "ccc"].iter().collect();
+    // cfg! macro doesn't work for this case it seems so we have this
+    // redundant use of the cfg directive instead
+    {
+        #[cfg(target_family = "unix")]
+        use std::os::unix::fs;
+        #[cfg(target_family = "windows")]
+        use std::os::windows::fs;
+        #[cfg(target_family = "unix")]
+        fs::symlink("mmm.txt", &bbb)?;
+        #[cfg(target_family = "unix")]
+        fs::symlink("mmm.txt", &ccc)?;
+        #[cfg(target_family = "windows")]
+        fs::symlink_file("mmm.txt", &bbb)?;
+        #[cfg(target_family = "windows")]
+        fs::symlink_file("mmm.txt", &ccc)?;
+    }
+    // take a snapshot of the test data
+    let snap1_sha = take_snapshot(Path::new(basepath), None, &dbase)?;
+    let snapshot1 = dbase.get_snapshot(&snap1_sha)?.unwrap();
+    assert_eq!(snapshot1.file_count, 1);
+    // replace the links with files and directories
+    fs::remove_file(&bbb)?;
+    fs::write(&bbb, b"bored baby baboons bathing")?;
+    fs::remove_file(&ccc)?;
+    let ccc: PathBuf = [basepath, "ccc", "ccc.txt"].iter().collect();
+    fs::create_dir(ccc.parent().unwrap())?;
+    fs::write(&ccc, b"crazy cat clawing chairs")?;
+    let snap2_sha = take_snapshot(Path::new(basepath), Some(snap1_sha.clone()), &dbase)?;
+    // compute the differences
+    let iter = find_changed_files(&dbase, snap1_sha.clone(), snap2_sha.clone())?;
+    let changed: Vec<Result<ChangedFile, Error>> = iter.collect();
+    assert_eq!(changed.len(), 2);
+    assert_eq!(changed[0].as_ref().unwrap().path, Path::new("./bbb"));
+    assert_eq!(changed[1].as_ref().unwrap().path, Path::new("./ccc/ccc.txt"));
+    Ok(())
+}
