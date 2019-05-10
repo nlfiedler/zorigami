b5da6dd305f1d6eebd0f7c0529e6bafdcb253a34
Nathan Fiedler <nathanfiedler@fastmail.fm>
2019-05-09 18:12:14 -0700

    Split GraphQL schema into its own module

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,19 @@
 //
 // Copyright (c) 2019 Nathan Fiedler
 //
-use juniper::{graphql_object, EmptyMutation, FieldResult};
 use juniper_warp;
 use log::info;
 use pretty_env_logger;
 use warp::{self, Filter};
 
-struct Context;
-
-// To make our context usable by Juniper, we have to implement a marker trait.
-impl juniper::Context for Context {}
-
-struct Query;
-
-graphql_object!(Query: Context |&self| {
-
-    /// Returns the version of the API as a string.
-    field apiVersion() -> &str {
-        "1.0"
-    }
-
-    /// Returns a greeting for the given name.
-    field hello(&executor, name: String) -> FieldResult<String> {
-        // Get the context from the executor.
-        // let context = executor.context();
-        // Get a db connection.
-        // let connection = context.pool.get_connection()?;
-        // Execute a db query.
-        // Note the use of `?` to propagate errors.
-        // let human = connection.find_human(&id)?;
-        // Return the result.
-        let mut result = String::from("Hello, ");
-        result.push_str(&name);
-        Ok(result)
-    }
-});
-
-// A root schema consists of a query and a mutation.
-// Request queries can be executed against a RootNode.
-type Schema = juniper::RootNode<'static, Query, EmptyMutation<Context>>;
+mod schema;
 
 pub fn main() {
     pretty_env_logger::init();
-    let schema = Schema::new(Query, EmptyMutation::new());
-    let state = warp::any().map(move || Context {});
+    let schema = schema::create_schema();
+    let state = warp::any().map(move || ());
     let graphql_filter = juniper_warp::make_graphql_filter(schema, state.boxed());
-    info!("listening on http://localhost:3030/...");
+    info!("listening on http://localhost:8080/...");
     warp::serve(
         warp::get2()
             .and(warp::path("graphiql"))
