9270ffb0f79deae53b5f0114facf78c262cf9503
Nathan Fiedler <nathanfiedler@fastmail.fm>
2019-06-14 23:52:36 -0700

    Basic webui using ReasonReact, bulma, fontawesome

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+const { exec } = require('child_process')
+const del = require('del')
+const gulp = require('gulp')
+const gulpif = require('gulp-if')
+const uglify = require('gulp-uglify')
+const webpack = require('webpack-stream')
+
+let production = process.env.NODE_ENV === 'production'
+
+gulp.task('make:bsb', (cb) => {
+  exec('npx bsb -make-world', (err, stdout, stderr) => {
+    console.info(stdout)
+    console.error(stderr)
+    cb(err)
+  })
+})
+
+gulp.task('webpack', () => {
+  return gulp.src('lib/js/src/Index.bs.js')
+    .pipe(webpack({
+      mode: production ? 'production' : 'development',
+      output: {
+        filename: 'main.js'
+      }
+    }))
+    .pipe(gulpif(production, uglify()))
+    .pipe(gulp.dest('public/javascripts'))
+})
+
+gulp.task('clean:bsb', (cb) => {
+  exec('npx bsb -clean-world', (err, stdout, stderr) => {
+    console.info(stdout)
+    console.error(stderr)
+    cb(err)
+  })
+})
+
+gulp.task('clean:js', () => {
+  return del(['public/javascripts/main.js'])
+})
+
+gulp.task('clean', gulp.series('clean:bsb', 'clean:js'))
+gulp.task('default', gulp.series('make:bsb', 'webpack'))
