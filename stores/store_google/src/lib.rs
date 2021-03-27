e33078367b25181af4bbeb518ef5c9409d949b90
Nathan Fiedler <nathanfiedler@fastmail.fm>
2021-03-26 18:44:36 -0700

    fix: fix build on Apple M1 hardware
    Using the aarch64 target with latest Rust (1.51) the application now builds
    correctly and tests successfully. Had to update the RocksDB version to
    latest git master, and replace the Google Cloud Storage crate with another
    similar crate that uses the latest versions of crates.
    
    cargo test passes

--- stores/store_google/src/lib.rs
+++ stores/store_google/src/lib.rs
@@ -1,15 +1,28 @@
 //
 // Copyright (c) 2020 Nathan Fiedler
 //
+//
+// If storage_v1_types crate is not yet published to crates.io, then
+// grab the generated code from example_crates/gcs_example in the
+// https://github.com/dermesser/async-google-apis repository.
+//
+mod storage_v1_types;
+use async_google_apis_common::{
+    yup_oauth2::{self, authenticator::Authenticator},
+    ApiError, DownloadResult,
+};
 use failure::{err_msg, Error};
-use google_storage1::{Bucket, Object, Storage};
+use hyper::client::HttpConnector;
 use hyper::Client;
+use hyper_rustls::HttpsConnector;
 use std::collections::HashMap;
 use std::path::Path;
+use std::sync::Arc;
 use store_core::Coordinates;
-use yup_oauth2::ServiceAccountAccess;
 
-type StorageHub = Storage<Client, ServiceAccountAccess<Client>>;
+type HttpClient = Client<HttpsConnector<HttpConnector>>;
+type AuthConnector = Authenticator<HttpsConnector<HttpConnector>>;
+type StorageHub = (HttpClient, Arc<AuthConnector>);
 
 #[derive(Debug)]
 pub struct GoogleStore {
@@ -40,179 +53,268 @@ impl GoogleStore {
         })
     }
 
-    fn connect(&self) -> Result<StorageHub, Error> {
-        match yup_oauth2::service_account_key_from_file(&self.credentials) {
-            Ok(client_secret) => {
-                let access = ServiceAccountAccess::new(
-                    client_secret,
-                    Client::with_connector(hyper::net::HttpsConnector::new(
-                        hyper_rustls::TlsClient::new(),
-                    )),
-                );
-                let hub = Storage::new(
-                    Client::with_connector(hyper::net::HttpsConnector::new(
-                        hyper_rustls::TlsClient::new(),
-                    )),
-                    access,
-                );
-                Ok(hub)
-            }
-            Err(err) => {
-                return Err(err_msg(format!(
-                    "error reading account key {}: {:?}",
-                    self.credentials, err
-                )));
-            }
-        }
+    async fn connect(&self) -> Result<StorageHub, Error> {
+        let conn = HttpsConnector::with_native_roots();
+        let https_client = Client::builder().build(conn);
+        let account_key = yup_oauth2::read_service_account_key(&self.credentials).await?;
+        let authenticator = yup_oauth2::ServiceAccountAuthenticator::builder(account_key)
+            .hyper_client(https_client.clone())
+            .build()
+            .await?;
+        Ok((https_client, Arc::new(authenticator)))
     }
 
-    pub fn store_pack(
+    pub fn store_pack_sync(
         &self,
         packfile: &Path,
         bucket: &str,
         object: &str,
     ) -> Result<Coordinates, Error> {
-        let hub = self.connect()?;
+        block_on(self.store_pack(packfile, bucket, object))
+    }
+
+    pub async fn store_pack(
+        &self,
+        packfile: &Path,
+        bucket: &str,
+        object: &str,
+    ) -> Result<Coordinates, Error> {
+        let hub = self.connect().await?;
         // the bucket must exist before receiving objects
-        create_bucket(&hub, &self.project, bucket, &self.region, &self.storage)?;
-        let mut req = Object::default();
-        req.name = Some(object.to_owned());
-        let result = hub.objects().insert(req, bucket).upload(
-            std::fs::File::open(packfile).unwrap(),
-            "application/octet-stream".parse().unwrap(),
-        );
+        create_bucket(&hub, &self.project, bucket, &self.region, &self.storage).await?;
+        let mut params = storage_v1_types::ObjectsInsertParams::default();
+        params.bucket = bucket.into();
+        params.name = Some(object.into());
+        let f = tokio::fs::OpenOptions::new()
+            .read(true)
+            .open(packfile)
+            .await?;
+        let svc = storage_v1_types::ObjectsService::new(hub.0, hub.1);
+        let obj = storage_v1_types::Object::default();
+        let result = match svc.insert_resumable_upload(&params, &obj).await {
+            Ok(mut upload) => match upload.set_max_chunksize(1024 * 1024 * 4) {
+                Ok(_) => upload.upload_file(f).await,
+                Err(err) => Err(err),
+            },
+            Err(err) => Err(err),
+        };
         // storing the same object twice is not treated as an error
-        if let Err(error) = result {
-            return Err(err_msg(format!("{:?}", error)));
-        }
-        let loc = Coordinates::new(&self.store_id, bucket, object);
-        Ok(loc)
-    }
-
-    pub fn retrieve_pack(&self, location: &Coordinates, outfile: &Path) -> Result<(), Error> {
-        let hub = self.connect()?;
-        let result = hub
-            .objects()
-            .get(&location.bucket, &location.object)
-            // the magic argument indicating to download the contents
-            .param("alt", "media")
-            .doit();
         match result {
-            Err(error) => Err(err_msg(format!("{:?}", error))),
-            Ok(mut response) => {
-                let mut file = std::fs::File::create(outfile)?;
-                // hyper v0.10 Response implements Read trait
-                std::io::copy(&mut response.0, &mut file)?;
-                Ok(())
+            Err(error) => return Err(err_msg(format!("{:?}", error))),
+            Ok(object) => {
+                if let Some(hash) = object.md5_hash.as_ref() {
+                    let decoded = base64::decode(hash)?;
+                    let md5 = md5sum_file(packfile)?;
+                    if !md5.eq(&decoded) {
+                        return Err(err_msg("returned md5_hash does not match MD5 of pack file"));
+                    }
+                }
             }
+        }
+        Ok(Coordinates::new(&self.store_id, bucket, object))
+    }
+
+    pub fn retrieve_pack_sync(&self, location: &Coordinates, outfile: &Path) -> Result<(), Error> {
+        block_on(self.retrieve_pack(location, outfile))
+    }
+
+    pub async fn retrieve_pack(&self, location: &Coordinates, outfile: &Path) -> Result<(), Error> {
+        let hub = self.connect().await?;
+        let svc = storage_v1_types::ObjectsService::new(hub.0, hub.1);
+        let mut gparams = storage_v1_types::StorageParams::default();
+        // the magic argument indicating to download the contents
+        gparams.alt = Some(storage_v1_types::StorageParamsAlt::Media);
+        let mut params = storage_v1_types::ObjectsGetParams::default();
+        params.storage_params = Some(gparams);
+        params.bucket = location.bucket.clone();
+        params.object = location.object.clone();
+        let mut f = tokio::fs::OpenOptions::new()
+            .write(true)
+            .create(true)
+            .open(outfile)
+            .await?;
+        match svc.get(&params).await {
+            Ok(mut download) => match download.do_it(Some(&mut f)).await {
+                Ok(result) => match result {
+                    DownloadResult::Downloaded => Ok(()),
+                    DownloadResult::Response(foo) => {
+                        Err(err_msg(format!("download response: {:?}", foo)))
+                    }
+                },
+                Err(err) => Err(err_msg(format!("{:?}", err))),
+            },
+            Err(err) => Err(err_msg(format!("{:?}", err))),
         }
     }
 
-    pub fn list_buckets(&self) -> Result<Vec<String>, Error> {
-        let hub = self.connect()?;
+    pub fn list_buckets_sync(&self) -> Result<Vec<String>, Error> {
+        block_on(self.list_buckets())
+    }
+
+    pub async fn list_buckets(&self) -> Result<Vec<String>, Error> {
+        let hub = self.connect().await?;
         let mut results: Vec<String> = Vec::new();
+        let svc = storage_v1_types::BucketsService::new(hub.0, hub.1);
+        let mut params = storage_v1_types::BucketsListParams::default();
         let mut page_token: Option<String> = None;
+        params.project = self.project.clone();
         loop {
-            let mut call = hub.buckets().list(&self.project);
-            if let Some(token) = page_token {
-                call = call.page_token(&token);
+            if page_token.is_some() {
+                params.page_token = page_token.take();
             }
-            let result = call.doit();
-            match result {
-                Err(error) => return Err(err_msg(format!("{:?}", error))),
-                Ok(response) => {
-                    if let Some(buckets) = response.1.items {
-                        // only consider named buckets (no guarantee they have one)
-                        for entry in buckets {
-                            if let Some(name) = entry.name {
-                                results.push(name);
+            match svc.list(&params).await {
+                Ok(buckets) => {
+                    if let Some(bucks) = buckets.items.as_ref() {
+                        // Only consider named buckets; this is no guarantee
+                        // that they have one, despite the API requiring that
+                        // names be provided when creating them.
+                        for bucket in bucks.iter() {
+                            if let Some(name) = bucket.name.as_ref() {
+                                results.push(name.to_owned());
                             }
                         }
                     }
-                    if response.1.next_page_token.is_none() {
+                    if buckets.next_page_token.is_none() {
                         break;
                     }
-                    page_token = response.1.next_page_token;
+                    page_token = buckets.next_page_token;
                 }
+                Err(err) => return Err(err_msg(format!("{:?}", err))),
             }
         }
         Ok(results)
     }
 
-    pub fn list_objects(&self, bucket: &str) -> Result<Vec<String>, Error> {
-        let hub = self.connect()?;
+    pub fn list_objects_sync(&self, bucket: &str) -> Result<Vec<String>, Error> {
+        block_on(self.list_objects(bucket))
+    }
+
+    pub async fn list_objects(&self, bucket: &str) -> Result<Vec<String>, Error> {
+        let hub = self.connect().await?;
         let mut results: Vec<String> = Vec::new();
+        let svc = storage_v1_types::ObjectsService::new(hub.0, hub.1);
+        let mut params = storage_v1_types::ObjectsListParams::default();
         let mut page_token: Option<String> = None;
+        params.bucket = bucket.into();
         loop {
-            let mut call = hub.objects().list(bucket);
-            if let Some(token) = page_token {
-                call = call.page_token(&token);
+            if page_token.is_some() {
+                params.page_token = page_token.take();
             }
-            let result = call.doit();
-            match result {
-                Err(error) => return Err(err_msg(format!("{:?}", error))),
-                Ok(response) => {
-                    if let Some(objects) = response.1.items {
-                        // only consider named objects (no guarantee they have one)
-                        for entry in objects {
-                            if let Some(name) = entry.name {
-                                results.push(name);
+            match svc.list(&params).await {
+                Ok(objects) => {
+                    if let Some(objs) = objects.items.as_ref() {
+                        // Only consider named objects; this is no guarantee
+                        // that they have one, despite the API requiring that
+                        // names be provided when uploading them.
+                        for object in objs.iter() {
+                            if let Some(name) = object.name.as_ref() {
+                                results.push(name.to_owned());
                             }
                         }
                     }
-                    if response.1.next_page_token.is_none() {
+                    if objects.next_page_token.is_none() {
                         break;
                     }
-                    page_token = response.1.next_page_token;
+                    page_token = objects.next_page_token;
                 }
+                Err(err) => return Err(err_msg(format!("{:?}", err))),
             }
         }
         Ok(results)
     }
 
-    pub fn delete_object(&self, bucket: &str, object: &str) -> Result<(), Error> {
-        let hub = self.connect()?;
-        let result = hub.objects().delete(bucket, object).doit();
-        if let Err(error) = result {
-            return Err(err_msg(format!("{:?}", error)));
+    pub fn delete_object_sync(&self, bucket: &str, object: &str) -> Result<(), Error> {
+        block_on(self.delete_object(bucket, object))
+    }
+
+    pub async fn delete_object(&self, bucket: &str, object: &str) -> Result<(), Error> {
+        let hub = self.connect().await?;
+        let svc = storage_v1_types::ObjectsService::new(hub.0, hub.1);
+        let mut params = storage_v1_types::ObjectsDeleteParams::default();
+        params.bucket = bucket.into();
+        params.object = object.into();
+        if let Err(err) = svc.delete(&params).await {
+            return Err(err_msg(format!("{:?}", err)));
         }
         Ok(())
     }
 
-    pub fn delete_bucket(&self, bucket: &str) -> Result<(), Error> {
-        let hub = self.connect()?;
-        let result = hub.buckets().delete(bucket).doit();
-        if let Err(error) = result {
-            return Err(err_msg(format!("{:?}", error)));
+    pub fn delete_bucket_sync(&self, bucket: &str) -> Result<(), Error> {
+        block_on(self.delete_bucket(bucket))
+    }
+
+    pub async fn delete_bucket(&self, bucket: &str) -> Result<(), Error> {
+        let hub = self.connect().await?;
+        let svc = storage_v1_types::BucketsService::new(hub.0, hub.1);
+        let mut params = storage_v1_types::BucketsDeleteParams::default();
+        params.bucket = bucket.into();
+        if let Err(err) = svc.delete(&params).await {
+            return Err(err_msg(format!("{:?}", err)));
         }
         Ok(())
     }
 }
 
 /// Ensure the named bucket exists.
-pub fn create_bucket(
+async fn create_bucket(
     hub: &StorageHub,
     project_id: &str,
     name: &str,
     region: &Option<String>,
     storage_class: &Option<String>,
 ) -> Result<(), Error> {
-    let mut req = Bucket::default();
-    req.name = Some(name.to_owned());
-    req.location = region.to_owned();
-    req.storage_class = storage_class.to_owned();
-    let result = hub.buckets().insert(req, project_id).doit();
-    if let Err(error) = result {
-        match error {
-            google_storage1::Error::BadRequest(response) => match response.error.code {
-                // same bucket name already exists which is fine
-                409 => return Ok(()),
-                _ => return Err(err_msg(format!("unhandled response {:?}", response))),
-            },
-            _ => return Err(err_msg(format!("{:?}", error))),
+    let (https_client, authenticator) = hub.clone();
+    let svc = storage_v1_types::BucketsService::new(https_client, authenticator);
+    let mut params = storage_v1_types::BucketsInsertParams::default();
+    params.project = project_id.to_owned();
+    let mut bucket = storage_v1_types::Bucket::default();
+    bucket.name = Some(name.to_owned());
+    bucket.location = region.to_owned();
+    bucket.storage_class = storage_class.to_owned();
+    if let Err(error) = svc.insert(&params, &bucket).await {
+        match error.downcast::<ApiError>() {
+            Ok(err) => {
+                match err {
+                    ApiError::HTTPResponseError(code, _) => match code {
+                        // bucket with the same name already exists
+                        hyper::StatusCode::CONFLICT => return Ok(()),
+                        _ => return Err(err_msg(format!("unhandled response {:?}", err))),
+                    },
+                    _ => return Err(err.into()),
+                }
+            }
+            Err(err) => return Err(err_msg(format!("{:?}", err))),
         }
     }
     Ok(())
+}
+
+/// Compute the MD5 digest of the given file.
+fn md5sum_file(infile: &Path) -> Result<Vec<u8>, Error> {
+    use md5::{Digest, Md5};
+    let mut file = std::fs::File::open(infile)?;
+    let mut hasher = Md5::new();
+    std::io::copy(&mut file, &mut hasher)?;
+    let digest: Vec<u8> = hasher.finalize()[..].into();
+    Ok(digest)
+}
+
+/// Run the given future either on the current runtime or on a newly created
+/// single-threaded future executor.
+fn block_on<F: core::future::Future>(future: F) -> F::Output {
+    if let Ok(handle) = tokio::runtime::Handle::try_current() {
+        handle.block_on(future)
+    } else {
+        // Build the simplest and lightest runtime we can, while still enabling
+        // us to wait for this future (and everything it spawns) to complete
+        // synchronously. Must enable the io and time features otherwise the
+        // runtime does not really start.
+        let runtime = tokio::runtime::Builder::new_current_thread()
+            .enable_all()
+            .build()
+            .unwrap();
+        runtime.block_on(future)
+    }
 }
 
 #[cfg(test)]
@@ -270,7 +372,7 @@ mod tests {
         let bucket = "747267d56e7057118a9aa40c24c1730f".to_owned();
         let object = "39c6061a56b7711f92c6ccd2047d47fdcc1609c1".to_owned();
         let packfile = Path::new("../../test/fixtures/lorem-ipsum.txt");
-        let result = source.store_pack(packfile, &bucket, &object);
+        let result = source.store_pack_sync(packfile, &bucket, &object);
         assert!(result.is_ok());
         let location = result.unwrap();
         assert_eq!(location.store, "google1");
@@ -278,14 +380,14 @@ mod tests {
         assert_eq!(location.object, object);
 
         // check for bucket(s) being present
-        let result = source.list_buckets();
+        let result = source.list_buckets_sync();
         assert!(result.is_ok());
         let buckets = result.unwrap();
         assert!(!buckets.is_empty());
         assert!(buckets.contains(&bucket));
 
         // check for object(s) being present
-        let result = source.list_objects(&bucket);
+        let result = source.list_objects_sync(&bucket);
         assert!(result.is_ok());
         let listing = result.unwrap();
         assert!(!listing.is_empty());
@@ -294,7 +396,7 @@ mod tests {
         // retrieve the file and verify by checksum
         let outdir = tempdir().unwrap();
         let outfile = outdir.path().join("restored.txt");
-        let result = source.retrieve_pack(&location, &outfile);
+        let result = source.retrieve_pack_sync(&location, &outfile);
         assert!(result.is_ok());
         let md5sum = store_core::md5sum_file(&outfile).unwrap();
         #[cfg(target_family = "unix")]
@@ -308,13 +410,13 @@ mod tests {
 
         // remove all objects from all buckets, and the buckets, too
         for bucket in buckets {
-            let result = source.list_objects(&bucket);
+            let result = source.list_objects_sync(&bucket);
             assert!(result.is_ok());
             let objects = result.unwrap();
             for obj in objects {
-                source.delete_object(&bucket, &obj).unwrap();
+                source.delete_object_sync(&bucket, &obj).unwrap();
             }
-            source.delete_bucket(&bucket).unwrap();
+            source.delete_bucket_sync(&bucket).unwrap();
         }
     }
 }
