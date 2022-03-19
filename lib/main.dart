ee120977093e2685ad4c83b6ca557a6fbf25dcb9
Nathan Fiedler <nathanfiedler@fastmail.fm>
2022-03-19 16:22:24 -0700

    fix: add form builder localization delegate

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@
 //
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
 import 'package:zorigami/features/backup/preso/screens/data_sets_screen.dart';
 import 'package:zorigami/features/backup/preso/screens/pack_stores_screen.dart';
 import 'package:zorigami/features/browse/preso/screens/home_screen.dart';
@@ -10,10 +11,12 @@ import 'package:zorigami/features/browse/preso/screens/database_restore_screen.d
 import 'package:zorigami/features/browse/preso/screens/restore_screen.dart';
 
 void main() {
-  runApp(ProviderScope(child: MyApp()));
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -31,6 +34,9 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.black.withOpacity(0),
         ),
       ),
+      localizationsDelegates: const [
+        FormBuilderLocalizations.delegate,
+      ],
     );
   }
 }
